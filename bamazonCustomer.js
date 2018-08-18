411f11fd5d98d2c152eac6ab2db00fc7b53093c4
Thorley <thorley.eric@gmail.com>
2018-08-18 15:34:00 -0400

    complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -60,26 +60,39 @@ function inquirerStart(){
         }
     ])
     .then(function(answers){
-        console.log("ID Prompt: " + answers.idPrompt ,"Quantity Prompt: " + answers.quantityPrompt);
-        connection.query(
-            "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
-            [              
-                answers.quantityPrompt, 
-                answers.idPrompt  
-            ],
-            function(err, res) {
+        connection.query("SELECT stock_quantity FROM products WHERE item_id = ?",[answers.idPrompt], function(err, res){
             if (err) throw err;
-        });
-        connection.query("SELECT * FROM products WHERE item_id =?", [answers.idPrompt], function (err, res) {
-            if (err) throw err;
-            for (x in res){
-            console.log("\nThe cost of your purchase is: " + ((answers.quantityPrompt) * (res[x].price)).toFixed(2) + "\n");
-            console.log("Thank you for shopping! You're our favourite customer!" + "\n\n"+
-            "-----------------------------------------------------------------------------------------------------"+"\n\n");
-            setTimeout(displayEverything, 2000);
+            if (res.length === 0)
+                console.log("Please select valid Item ID");
+            
+            let remainingStock = res[0].stock_quantity
+            let demand = answers.quantityPrompt
+            if ((remainingStock < demand)===true){
+                console.log("Insufficient stock to fulfill order! Only "+ remainingStock + " left");
+                return;
+                
+                } else {
+        
+                connection.query(
+                    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+                    [              
+                        answers.quantityPrompt, 
+                        answers.idPrompt  
+                    ],
+                    function(err, res) {
+                    if (err) throw err;
+                });
+                connection.query("SELECT * FROM products WHERE item_id =?", [answers.idPrompt], function (err, res) {
+                    if (err) throw err;
+                    for (x in res){
+                    console.log("\nThe cost of your purchase is: " + ((answers.quantityPrompt) * (res[x].price)).toFixed(2) + "\n");
+                    console.log("Thank you for shopping! You're our favourite customer!" + "\n\n"+
+                    "-----------------------------------------------------------------------------------------------------"+"\n\n");
+                    setTimeout(displayEverything, 2000);
+                    }
+                });
             }
         });
-        
     });
 }
 
